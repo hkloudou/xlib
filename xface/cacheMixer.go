021f4c4ef1ad208d9404dac583b5d15db2180051
sample <global-config@example.com>
2021-12-29 20:52:22 +0800

    fx

---
+++ xface/cacheMixer.go
@@ -0,0 +1,37 @@
+package xface
+
+import (
+	"fmt"
+	"time"
+)
+
+type cacherMixer[T any] struct {
+	c1 Cacher[T] //l1 cacher
+	c2 Cacher[T] //l2 cacher
+}
+
+func NewCacheMixer[T any](c1 Cacher[T], c2 Cacher[T]) Cacher[T] {
+	return &cacherMixer[T]{}
+}
+
+func (m *cacherMixer[T]) Get(key string) (*T, error) {
+	if tmp, err := m.c1.Get(key); err == nil {
+		return tmp, nil
+	} else if tmp, err := m.c2.Get(key); err == nil {
+		m.c1.Set(key, 0, tmp)
+		return tmp, nil
+	}
+	return nil, fmt.Errorf("not found")
+}
+
+func (m *cacherMixer[T]) Set(key string, ttl time.Duration, obj *T) error {
+	m.c2.Set(key, ttl, obj)
+	m.c1.Set(key, ttl, obj)
+	return nil
+}
+
+func (m *cacherMixer[T]) Del(key ...string) error {
+	m.c2.Del(key...)
+	m.c1.Del(key...)
+	return nil
+}
