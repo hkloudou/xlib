31c5128a777974c31bba2caddbc13c7058a0ff28
sample <global-config@example.com>
2021-12-31 00:57:38 +0800

    auto commit

--- xface/flag.go
+++ xface/flag.go
@@ -3,12 +3,15 @@ package xface
 import (
 	"fmt"
 	"reflect"
+	"strings"
 	"sync"
 
 	"github.com/hkloudou/xlib/xflag"
 )
 
 var _flags = sync.Map{}
+var _list = []string{}
+var _lk = &sync.RWMutex{} //主要对_list
 
 type FlagConfig[T any] interface {
 	Flags() []xflag.Flag
@@ -17,7 +20,12 @@ type FlagConfig[T any] interface {
 }
 
 func FlagRegister[T any](name string, obj FlagConfig[T]) {
+	_lk.Lock()
+	defer _lk.Unlock()
 	_flags.Store(name, obj)
+	if !strings.Contains(","+strings.Join(_list, ",")+",", name) {
+		_list = append(_list, name)
+	}
 }
 
 func FlagGet[T any](name string) (*T, error) {
@@ -30,16 +38,25 @@ func FlagGet[T any](name string) (*T, error) {
 	}
 }
 
+//FlagAction 让Action 有序执行，这样在一些场景下实现智能依赖
 func FlagAction(c *xflag.Context) (err error) {
-	_flags.Range(func(key, value any) bool {
-		mt := reflect.ValueOf(value).MethodByName("Action")
-		res := mt.Call([]reflect.Value{reflect.ValueOf(c)})
-		if !res[0].IsNil() {
-			err = res[0].Interface().(error)
-			return false
+	_lk.RLock()
+	defer _lk.RUnlock()
+	for i := 0; i < len(_list); i++ {
+		name := _list[i]
+		if value, found := _flags.Load(name); !found {
+			continue
+		} else {
+			mt := reflect.ValueOf(value).MethodByName("Action")
+			res := mt.Call([]reflect.Value{reflect.ValueOf(c)})
+			if !res[0].IsNil() {
+				err = res[0].Interface().(error)
+				if err != nil {
+					return
+				}
+			}
 		}
-		return true
-	})
+	}
 	return
 }
 
