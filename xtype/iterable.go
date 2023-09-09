b7fd8192eca68d8c8c9a0ec8b32c97634146159e
sample <sample@example.com>
2023-09-09 17:06:31 +0800

    auto commit

---
+++ xtype/iterable.go
@@ -0,0 +1,15 @@
+package xtype
+
+// type Iterable[E comparable] struct {
+// }
+
+// func (m *Iterable[E]) Where(test func(element E) bool) {
+
+// }
+
+// func (m *Iterable[E]) Map(test func(element E) string,X T) Iterable[T] {
+
+// }
+
+// type WhereIterable struct {
+// }
